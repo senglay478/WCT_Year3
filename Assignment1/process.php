a17a4dbed09a77999d29c50f5170f60f3ebbacad
senglay478 <senglay.poch@gmail.com>
2025-03-15 22:49:54 +0700

    new

--- Assignment1/process.php
+++ Assignment1/process.php
@@ -1,16 +1,24 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
-
-    if (!empty($name) && !empty($email)) {
-        echo "<h2>Form Submitted Successfully!</h2>";
+    $password = htmlspecialchars($_POST['password']);
+    $confirmpassword = htmlspecialchars($_POST['confirmpassword']);
+ 
+    if (empty($name) || empty($email) || empty($password) || empty($confirmpassword)) {
+        echo "<h2 class='text-red-500 font-bold text-center'>Error: All fields must be filled.</h2>";
+    } 
+    elseif ($password !== $confirmpassword) {
+        echo "<h2 class='text-red-500 font-bold text-center'>Error: Passwords do not match.</h2>";
+    } 
+    // If all conditions are met, display success message
+    else {
+        echo "<h2 class='text-green-500 font-bold text-center'>Account Created</h2>";
         echo "<p><strong>Name:</strong> $name</p>";
         echo "<p><strong>Email:</strong> $email</p>";
-    } else {
-        echo "<h2>Error: All fields are required.</h2>";
     }
 } else {
-    echo "<h2>Invalid Request</h2>";
+    echo "<h2 class='text-red-500 font-bold text-center'>Invalid Request</h2>";
 }
 ?>
